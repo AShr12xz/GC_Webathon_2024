ddc2a5f9f766b7412e5612796c4c8f213a933e2d
AShr12xz <122085103+AShr12xz@users.noreply.github.com>
2024-03-23 01:46:43 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='flex justify-center'>
+
+      <h1 className=' mt-10 text-7xl text-red-900  font-mono'>ERP</h1>
+ 
+    </div>
+  );
+}
+
+export default App;
