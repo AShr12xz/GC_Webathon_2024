06e6a7d9939c9dfa1ab328eb5bc2a5af1415a4d3
AShr12xz <122085103+AShr12xz@users.noreply.github.com>
2024-03-25 15:12:26 +0530

    Updated signToken function and add faculty-specific token generation in login

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -6,63 +6,76 @@ const error = require('../utils/error')
 const asyncCheck = require('../utils/asyncCheck')
 require("dotenv").config();
 
-const signToken = id => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '90d'
-    });
+const signToken = (id, user = 'student') => {
+  return jwt.sign({ id, user }, process.env.JWT_SECRET, {
+    expiresIn: '90d'
+  });
 };
 
 exports.signup = asyncCheck(async (req, res, next) => {
-    if (req.body.role === 'faculty') {
-        if (req.body.code != 'abcd') {
-            return next(new error('Wrong faculty code', 401))
-        }
+  if (req.body.role === 'faculty') {
+    if (req.body.code != 'abcd') {
+      return next(new error('Wrong faculty code', 401))
     }
-    const filteredBody = { ...req.body };
-    delete filteredBody.code;
-    console.log(filteredBody);
+  }
+  const filteredBody = { ...req.body };
+  delete filteredBody.code;
+  console.log(filteredBody);
 
-    if (req.body.role === 'student') {
-        const newStudent = await Student.create(filteredBody);
+  if (req.body.role === 'student') {
+    const newStudent = await Student.create(filteredBody);
+  }
+  else {
+    const newFaculty = await Faculty.create(filteredBody)
+  }
+
+  const newUser = await User.create(filteredBody);
+
+  const token = signToken(newUser._id);
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser
     }
-    else {
-        const newFaculty = await Faculty.create(filteredBody)
-    }
-
-    const newUser = await User.create(filteredBody);
-
-    const token = signToken(newUser._id);
-
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    });
+  });
 })
 
 exports.login = asyncCheck(async (req, res, next) => {
 
-    const { uniqueId, password } = req.body;
-    if (!uniqueId || !password)
-        return next(new error("All fields are required", 400));
+  const { uniqueId, password } = req.body;
+  if (!uniqueId || !password)
+    return next(new error("All fields are required", 400));
 
-    const user = await User.findOne({ uniqueId });
-    if (!user || !await user.comparePassword(password, user.password)) {
-        return next(new error("Incorrect email or password", 401));
-    }
+  const user = await User.findOne({ uniqueId });
+  if (!user || !await user.comparePassword(password, user.password)) {
+    return next(new error("Incorrect email or password", 401));
+  }
 
-    const token = signToken(user._id);
 
+  if (user.role === 'faculty') {
+    const facultyToken = signToken(user._id, 'faculty'); // Generate a faculty-specific token
     res.status(200).json({
-        status: 'success',
-        token,
-        data: {
-            user: user
-        }
+      status: 'success',
+      token: facultyToken, // Send faculty token
+      data: {
+        user: user
+      }
     });
-})
+  } else {
+    // Generate a regular token for non-faculty users
+    const token = signToken(user._id);
+    res.status(200).json({
+      status: 'success',
+      token,
+      data: {
+        user: user
+      }
+    })
+  }
+});
+
 
 module.exports.userVerification = (req, res) => {
   const { token } = req.body;
