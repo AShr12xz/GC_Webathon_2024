85b656e06b6c33fd972c925f356c8d2a74eee064
krutarthjankat <krutarth.jankat62@gmail.com>
2024-06-08 20:11:48 +0530

    backend deployment test 4

--- server/models/studentModel.js
+++ server/models/studentModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const studentSchema = new mongoose.Schema({
     name: {
